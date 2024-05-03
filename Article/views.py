b6a3233c7ee17ff72c397734586d826f259a1b54
Juhyeon Jeong <wwg6710@naver.com>
2024-05-03 17:12:13 +0900

    article crud & 유효성 검사(생성, 수정)

--- Article/views.py
+++ Article/views.py
@@ -1,24 +1,42 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework import status
 
 class ArticleListView(APIView):
 
     def get(self, request):
-        pass
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
 
     def post(self, request):
-        pass
-
+        #JSON으로 입력하는 데이터 받아오기
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ArticleDetailView(APIView):
 
     def get(self, request, pk):
-        pass
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def put(self, request, pk):
-        pass
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def delete(self, request, pk):
-        pass
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        data = {"delete": f"Article({pk}) is deleted."}
+        return Response(data, status=status.HTTP_201_CREATED)
 
 class CommentListView(APIView):
 
