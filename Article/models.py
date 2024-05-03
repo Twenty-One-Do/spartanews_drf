57aa5a9f26df59ab8c6d5a92c62591cb1e7a5372
Twenty-One-Do <dnjseh8962@gmail.com>
2024-05-03 13:26:01 +0900

    CREATE: DB Scheme
    - Article DB Scheme 생성.
    - User는 장고에서 기본적으로 제공하는 auth_user를 사용합니다.

--- Article/models.py
+++ Article/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    ARTICLE_TYPE = (('News', 'News'),
+                    ('Ask', 'Ask'),
+                    ('Show', 'Show'),
+                    ('GN_plus', 'GN_plus'),)
+    type = models.CharField(max_length=20, choices=ARTICLE_TYPE)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=300)
+    content = models.TextField()
+    likes_num = models.PositiveIntegerField(default=0)
+    comments_num = models.PositiveIntegerField(default=0)
+    alert = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    alert = models.BooleanField(default=False)
+    likes_num = models.PositiveIntegerField(default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+class Comment_Rel(models.Model):
+    parent = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='child_comments')
+    child = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='parent_comments')
+
+class Likes_Rel(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='like_users')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='like_articles')
+
+class Comment_Likes_Rel(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='like_users')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='like_comments')
--- Article/urls.py
+++ Article/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view()),
-    path('/<int:pk>', views.ArticleDetailView.as_view()),
-    path('/<int:pk>/comment/', views.CommentListView.as_view()),
-    path('/comment/<int:pk>', views.CommentDetailView.as_view()),
+    path('<int:pk>', views.ArticleDetailView.as_view()),
+    path('<int:pk>/comment/', views.CommentListView.as_view()),
+    path('comment/<int:pk>', views.CommentDetailView.as_view()),
 ]
