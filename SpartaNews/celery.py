d42a9e3336747d636550cb8116feb342f849eb74
Twenty-One-Do <dnjseh8962@gmail.com>
2024-05-07 20:45:21 +0900

    UPDATE: celery.py

--- SpartaNews/celery.py
+++ SpartaNews/celery.py
@@ -2,12 +2,12 @@ from __future__ import absolute_import, unicode_literals
 from celery import Celery
 import os
 
-from SpartaNews.settings import CELERY_BROKER_URL
+from django.conf import settings
 
 # Django 프로젝트의 settings 모듈을 Celery에게 알려줍니다.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpartaNews.settings')
 
-app = Celery('SpartaNews', broker=CELERY_BROKER_URL)
+app = Celery('SpartaNews', broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_RESULT_BACKEND)
 
 # namespace='CELERY'는 Celery 관련 설정을 Django의 설정 파일에서 가져오게끔 합니다.
 app.config_from_object('django.conf:settings', namespace='CELERY')
