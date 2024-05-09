f088cff5cc673d2462d81c49832446f724d3bcaf
Juhyeon Jeong <wwg6710@naver.com>
2024-05-09 10:38:43 +0900

    pip freeze

--- Summary/apps.py
+++ Summary/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SummaryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Summary'
+
+    def ready(self):
+        import nltk
+        nltk.download('punkt')
